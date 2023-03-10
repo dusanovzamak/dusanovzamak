f0376766996f1e51e4bc3a0f11443602afcc4937
dusanovzamak <127540904+dusanovzamak@users.noreply.github.com>
2023-03-10 18:44:51 +0100

    Add files for site

---
+++ konami.js
@@ -0,0 +1,42 @@
+// a key map of allowed keys
+var allowedKeys = {
+  77: 'm',
+  65: 'a',
+  84: 't',
+  73: 'i',
+  74: 'j',
+};
+
+// the 'official' Konami Code sequence
+var konamiCode = ['m', 'a', 't', 'i', 'j', 'a', 'm', 'a', 't', 'i', 'j', 'a'];
+
+// a variable to remember the 'position' the user has reached so far.
+var konamiCodePosition = 0;
+
+// add keydown event listener
+document.addEventListener('keydown', function(e) {
+  // get the value of the key code from the key map
+  var key = allowedKeys[e.keyCode];
+  // get the value of the required key from the konami code
+  var requiredKey = konamiCode[konamiCodePosition];
+
+  // compare the key with the required key
+  if (key == requiredKey) {
+
+    // move to the next key in the konami code sequence
+    konamiCodePosition++;
+
+    // if the last key is reached, activate cheats
+    if (konamiCodePosition == konamiCode.length) {
+      activateCheats();
+      konamiCodePosition = 0;
+    }
+  } else {
+    konamiCodePosition = 0;
+  }
+});
+
+function activateCheats() {
+
+  alert("gay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sexgay sex");
+}// JavaScript Document
